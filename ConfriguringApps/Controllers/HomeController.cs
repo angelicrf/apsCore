2ae5c965e44e0aa4cc8d71de6df7ffd8ef4a5413
angelicrf <angelicrf@gmail.com>
2020-06-20 18:14:02 -0700

    W localhost/middleware

---
+++ ConfriguringApps/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConfriguringApps.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfriguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UptimeService uptime;
+        public HomeController(UptimeService up) => uptime = up;
+        public IActionResult Index()
+                   => View(new Dictionary<string, string>
+                   {
+                       ["Message"] = "This is from the Home controller /Index",
+                       ["Uptime"] = $"{uptime.Uptime} millissecond"
+                   });
+    }
+}
