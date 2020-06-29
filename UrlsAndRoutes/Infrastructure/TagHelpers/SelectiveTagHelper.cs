482d2d6c9b67716f519ab73934022e71291f2c87
angelicrf <angelicrf@gmail.com>
2020-06-29 14:43:07 -0700

    viewContext - htmlAttributeNotBound - htmlTargetElement

---
+++ UrlsAndRoutes/Infrastructure/TagHelpers/SelectiveTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlsAndRoutes.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "showaction")]
+    public class SelectiveTagHelper : TagHelper
+    {
+        public string ShowAction { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!ViewContext.RouteData.Values["action"].ToString().Equals(ShowAction, StringComparison.OrdinalIgnoreCase))
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
---
+++ UrlsAndRoutes/Infrastructure/TagHelpers/TableCellTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace UrlsAndRoutes.Infrastructure.TagHelpers
+{
+
+        [HtmlTargetElement("td", Attributes = "wrap")]
+        public class TableCellTagHelper : TagHelper
+        {
+            public override void Process(TagHelperContext context,TagHelperOutput output)
+            {
+                output.PreContent.SetHtmlContent("<strong><i>");
+                output.PostContent.SetHtmlContent("</i></strong>");
+            }
+        }
+    
+}
