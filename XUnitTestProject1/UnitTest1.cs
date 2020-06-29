67911ffe03b77f698d802f15b96a77a031ec9d00
angelicrf <angelicrf@gmail.com>
2020-06-29 11:29:08 -0700

    test taghelper passed

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using Moq;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using UrlsAndRoutes.Infrastructure.TagHelpers;
 using UrlsAndRoutes.Models;
 using Xunit;
 
@@ -9,5 +12,20 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
+        [Fact]
+        public void TestTagHelper()
+        {
+            var context = new TagHelperContext(new TagHelperAttributeList(),new Dictionary<object, object>(),"myuniqueid");
+
+            var output = new TagHelperOutput("button",new TagHelperAttributeList(), (cache, encoder) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+           
+            var tagHelper = new ButtonTagHelper
+            {
+                BsButtonColor = "testValue"
+            };
+            tagHelper.Process(context, output);
+            
+            Assert.Equal($"btn btn-{tagHelper.BsButtonColor}",output.Attributes["class"].Value);
+        }
     }
 }
